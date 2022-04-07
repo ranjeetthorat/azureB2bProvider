d4c23fc44b6cb2116ed160cfcd7f179819883a7a
ranjeetthorat <80979748+ranjeetthorat@users.noreply.github.com>
2022-04-07 21:45:52 +0530

    nextauth file

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,63 +1,29 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/facebook"
-import GithubProvider from "next-auth/providers/github"
-import TwitterProvider from "next-auth/providers/twitter"
-import Auth0Provider from "next-auth/providers/auth0"
-// import AppleProvider from "next-auth/providers/apple"
-// import EmailProvider from "next-auth/providers/email"
+import AzureADB2CProvider from "next-auth/providers/azure-ad-b2c";
+import NextAuth, { NextAuthOptions } from 'next-auth';
 
-// For more information on each option (and a full list of options) go to
-// https://next-auth.js.org/configuration/options
-export default NextAuth({
-  // https://next-auth.js.org/configuration/providers/oauth
-  providers: [
-    /* EmailProvider({
-         server: process.env.EMAIL_SERVER,
-         from: process.env.EMAIL_FROM,
-       }),
-    // Temporarily removing the Apple provider from the demo site as the
-    // callback URL for it needs updating due to Vercel changing domains
-      
-    Providers.Apple({
-      clientId: process.env.APPLE_ID,
-      clientSecret: {
-        appleId: process.env.APPLE_ID,
-        teamId: process.env.APPLE_TEAM_ID,
-        privateKey: process.env.APPLE_PRIVATE_KEY,
-        keyId: process.env.APPLE_KEY_ID,
-      },
-    }),
-    */
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_ID,
-      clientSecret: process.env.TWITTER_SECRET,
-    }),
-    Auth0Provider({
-      clientId: process.env.AUTH0_ID,
-      clientSecret: process.env.AUTH0_SECRET,
-      issuer: process.env.AUTH0_ISSUER,
-    }),
-  ],
-  theme: {
-    colorScheme: "light",
-  },
-  callbacks: {
-    async jwt({ token }) {
-      token.userRole = "admin"
-      return token
-    },
-  },
-})
+const Tenant = process.env.tenant;
+const PolicySignUpSignIn = "B2C_1_susi";
+ const ClientID  = process.env.clientid
+
+const options = {
+providers: [
+  AzureADB2CProvider({
+    tenantId: Tenant,
+    clientId: ClientID,
+    primaryUserFlow: PolicySignUpSignIn,
+    
+    authorization: {
+      url: 'https://example.b2clogin.com/example/b2c_1_susi/oauth2/v2.0/authorize',
+      params: { 
+      scope: "offline_access openid " +ClientID,
+      redirect_uri: 'https://example.domain-dev.com',
+   } },
+   protection: ['pkce, state'],
+  }),
+  
+],
+}
+
+const NextAuthHandler = ( req, res ) => NextAuth( req, res, options );
+
+export default NextAuthHandler
